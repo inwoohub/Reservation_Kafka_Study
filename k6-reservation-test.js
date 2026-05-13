f6fa2cbe8ee2491f00af6d3d41bdb10534848c7f
inwoohub <dlsdndls@naver.com>
2026-05-13 10:34:05 +0900

    k6 : k6 부하테스트용 스크립트 추가

---
+++ k6-reservation-test.js
@@ -0,0 +1,44 @@
+/**
+ * k6 부하 테스트 스크립트입니다.
+ * 
+ * 대상 : 예매하기 API
+ * 
+ * 방법 : 랜덤하게 값을 채워넣어서 초당 100개씩 30초 동안
+ * 
+ * 실행 방법 : k6 run reservation-test.js
+ */
+
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  vus: 100,
+  duration: '30s',
+};
+
+export default function () {
+  const randomNum = Math.floor(Math.random() * 100000000);
+
+  const payload = JSON.stringify({
+    productId: 1,
+    quantity: 1,
+    buyerName: `buyer-${randomNum}`,
+    birthDate: `19990101`,
+    teamPassword: `${1000 + Math.floor(Math.random() * 9000)}`
+  });
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const res = http.post('http://localhost:8081/api/reservation', payload, params);
+
+  check(res, {
+    'status is 201': (r) => r.status === 201,
+    'response time < 500ms': (r) => r.timings.duration < 500,
+  });
+
+  sleep(1);
+}
